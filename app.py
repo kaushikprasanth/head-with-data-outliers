509e561ffffb99a0a172c645f590a229726cf73a
kaushikprasanth <kaushikprasanth94@yahoo.com>
2020-08-26 16:54:34 -0500

    Prod Deployment Changes

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
-
+import flask
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__)
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, server=server,external_stylesheets=external_stylesheets)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
+
 df = pd.DataFrame({
     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
     "Amount": [4, 1, 2, 2, 4, 5],
@@ -21,7 +21,7 @@ fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
 
-    html.Div(children='<h3>Dash: A web application framework for Python.</h3>'),
+    html.Div(children='Dash: A web application framework for Python'),
 
     dcc.Graph(
         id='example-graph',
